a51909df2192274a53245e78c055070b14e8952b
Ajayk93 <ajaynew83@gmail.com>
2024-08-09 11:25:28 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(0);
+  let counter = useRef(0);
+  
+  useLayoutEffect(()=>{
+    counter.current++;
+  })
+
+  const increment = () => {
+    setValue(value+1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>useRef value: {counter.current}</h2>
+      <h2>useState value: {value}</h2>
+      <button onClick={() => increment()}>click me</button>
     </div>
   );
 }
